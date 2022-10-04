9fd36dcb3a2cb07d04793cb06967dcc33816e0f2
Yazan Istatiyeh <79809251+YazanIST@users.noreply.github.com>
2022-10-04 16:48:34 +0300

    Create Trie.cpp

---
+++ Trie.cpp
@@ -0,0 +1,30 @@
+class Trie {
+private:
+    struct Node {
+        char alphabet;
+        bool exist;
+        vector<Node*> child;
+        Node(char a): alphabet(a), exist(false) {
+            child.assign(26, NULL);
+        }
+    };
+    
+    Node* root;
+ 
+public:
+    Trie() {
+        root = new Node('!');
+    }
+ 
+    void insert(string word) {
+        Node* cur = root;
+        for (int i = 0; i < (int) word.size(); i++) {
+            int alphaNum = word[i] - 'a';
+            if (cur -> child[alphaNum] == NULL) {
+                cur -> child[alphaNum] = new Node(word[i]);
+            }
+            cur = cur -> child[alphaNum];
+        }
+        cur -> exist = true;
+    }
+};
