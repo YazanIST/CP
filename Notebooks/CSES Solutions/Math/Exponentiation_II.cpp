c36d16e9243fef1778caaa1070f64f0171ee998c
Yazan Istatiyeh <79809251+YazanIST@users.noreply.github.com>
2022-12-07 17:03:03 +0300

    Delete Notebooks directory

--- Notebooks/CSES Solutions/Math/Exponentiation_II.cpp
+++
@@ -1,3 +0,0 @@
-// calculate values a^b^c modulo 1e9+7
-int r = power(b, c, MOD - 1);
-cout << power(a, r, MOD) << '\n';
--- Notebooks/TeamNotebook/String Algos/string_mod_int.cpp
+++
@@ -1,8 +0,0 @@
-int mod(string num, int a){
-    int ret=0;
- 
-    for(int i=0;i<num.length();i++)
-        ret=(ret*10+(int)num[i]-'0')%a;
- 
-    return ret;
-}
